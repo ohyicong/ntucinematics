98d6ccbd757cc8d819a842c35511264572469d08
Oh Yicong <ohyicong@hotmail.com>
2018-10-20 01:25:37 +0800

    updated with php

--- cart.php
+++ cart.php
@@ -266,15 +266,31 @@
 			<img src="./img/logo.jpg" style="width:80%;height:100%;">
 		</header>
 		<nav style="text-align:right;padding-right:0px">
-			<a href="./index.html" class="menu">Home</a>
+			<a href="./index.php" class="menu">Home</a>
 			<a href="./movies.html" class="menu">Movies</a>
 			<a href="./promotions.html" class="menu">Promotions</a>
-			<a href="./cart" class="menu" > Cart</a>
+			<a href="./cart.php" class="menu" > Cart</a>
 			<span class="account-box" style="float:right;">
-				<span id='account' class="menu" style="padding-right:0px"> 
-					Account
-				</span>
-				<span id="account-option" class="account-option" style="width:100%;text-align: center;"></span>
+				<?php
+					if (isset($_SESSION["useraccount"])){
+						echo "	<span id='account' class='menu' style='padding-right:0px'> 
+									Account
+								</span>
+								<span id='account-option' class='account-option' style='width:100%;text-align: center;''>
+									<a>Profile</a>
+									<a>Logout</a>
+								</span>	";		
+					}else{
+						echo"	<span id='account' class='menu' style='padding-right:0px'> 
+									Account
+								</span>
+								<span id='account-option' class='account-option' style='width:100%;text-align: center;''>
+									<a>Login</a>
+									<a>Register</a>
+								</span>	";
+					}
+				?>
+				
 			</span>			
 			<span class="dot" id="dot">0</span>
 		</nav>
